fa43c03c4f39db831914464b43aceccb626ade2e
Tejasbisen786 <bisentejas@gmail.com>
2023-08-19 10:01:34 +0530

    L-2 Basic React Project intialised

---
+++ L-2 Crete a React Project/newBasicApp/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './profile.css'
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <div className="box">
+<img  className="profileImg"  src=" https://www.pngitem.com/pimgs/m/22-220721_circled-user-male-type-user-colorful-icon-png.png" alt="" />
+      <h1>Tejas Bisen</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, aut. </p>
+      </div>
+      
+    </div>
+    
+    </>
+  )
+}
+
+export default App
