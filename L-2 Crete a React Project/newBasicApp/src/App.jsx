56ab483a1c44bdb6a10fefdea5834a491c6baa6d
Tejasbisen786 <bisentejas@gmail.com>
2023-08-19 13:42:13 +0530

    updated file with taking reference of L-3

--- L-2 Crete a React Project/newBasicApp/src/App.jsx
+++ L-2 Crete a React Project/newBasicApp/src/App.jsx
@@ -1,20 +1,26 @@
-
-import './profile.css'
+import "./profile.css";
+import  Chai from "./chai.jsx"
 function App() {
-
   return (
     <>
-    <div className="container">
-      <div className="box">
-<img  className="profileImg"  src=" https://www.pngitem.com/pimgs/m/22-220721_circled-user-male-type-user-colorful-icon-png.png" alt="" />
-      <h1>Tejas Bisen</h1>
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, aut. </p>
+      <div className="container">
+        <div className="box">
+          <img
+            className="profileImg"
+            src=" https://www.pngitem.com/pimgs/m/22-220721_circled-user-male-type-user-colorful-icon-png.png"
+            alt=""
+          />
+          <h1>Tejas Bisen</h1>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, aut.{" "}
+          </p> 
+        </div>
       </div>
-      
-    </div>
-    
+      <Chai/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+ 
