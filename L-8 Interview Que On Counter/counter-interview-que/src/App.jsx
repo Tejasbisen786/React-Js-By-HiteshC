877042e7aaf2abdef5b8b9c107ea7a5f806e0321
Tejasbisen786 <bisentejas@gmail.com>
2023-08-26 22:37:45 +0530

    Lecture 8 Added

---
+++ L-8 Interview Que On Counter/counter-interview-que/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Counter Value is : 80</h2>
+
+      <button style={{margin:20}}> Add Value</button>
+      <button> Remove Value</button>
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ L-8 Interview Que On Counter/reactInterview.md
@@ -0,0 +1,2 @@
+# A React Interview Question On Counter
+  - Question Related Counter
