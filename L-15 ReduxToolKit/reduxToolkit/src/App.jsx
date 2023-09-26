05ac0255952aad704b701d0f11b67773d065c0f6
Tejasbisen786 <bisentejas@gmail.com>
2023-09-26 12:44:04 +0530

    Redux Toolkit added

---
+++ L-15 ReduxToolKit/reduxToolkit/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+
+const imgPAth="https://images.pexels.com/photos/17169267/pexels-photo-17169267/free-photo-of-couple-standing-on-pier-and-looking-on-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  return (
+    <>
+      <h1 className="text-center text-2xl font-bold text-white bg-orange-500 p-5">
+        Redux Toolkit
+      </h1>
+      {/* <div className="flex "> 
+        <img src={imgPAth} alt="" className="w-[90vw] h-[90vh]" />
+      </div> */}
+    </>
+  );
+};
+
+export default App;
