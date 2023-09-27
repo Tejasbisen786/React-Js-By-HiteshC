baf975157ef30b2019441c782ae2ac5a934e9cff
Tejasbisen786 <bisentejas@gmail.com>
2023-09-27 12:15:58 +0530

    Redux Toolkit with todo app added

--- L-15 ReduxToolKit/reduxToolkit/src/App.jsx
+++ L-15 ReduxToolKit/reduxToolkit/src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import AddTodo from "./components/addTodo";
+import Todos from "./components/Todos";
 
 const App = () => {
-
-const imgPAth="https://images.pexels.com/photos/17169267/pexels-photo-17169267/free-photo-of-couple-standing-on-pier-and-looking-on-sea.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   return (
     <>
       <h1 className="text-center text-2xl font-bold text-white bg-orange-500 p-5">
         Redux Toolkit
       </h1>
-      {/* <div className="flex "> 
-        <img src={imgPAth} alt="" className="w-[90vw] h-[90vh]" />
-      </div> */}
+      <AddTodo />
+      <Todos />
     </>
   );
 };
