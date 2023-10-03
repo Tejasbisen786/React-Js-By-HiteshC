e1f4f0ddf1d3de4750a0415518082fae20bace7f
Tejasbisen786 <bisentejas@gmail.com>
2023-10-03 21:18:38 +0530

    Slug Value

--- Mega Project/mega-blog/src/components/Select.jsx
+++ Mega Project/mega-blog/src/components/Select.jsx
@@ -14,14 +14,14 @@ const Select = ({ options, label, className = "", ...props }, ref) => {
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
-        {options?.map((option)=>(
-            <option key={option} value={option}>
-                {option}
-            </option>
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
         ))}
       </select>
     </div>
   );
 };
 
-export default React.forwardRef (Select);
+export default React.forwardRef(Select);
---
+++ Mega Project/mega-blog/src/components/post-from/PostForm.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, Select, RTE } from "../index";
+import appwriteService from "../../appwrite/config";
+import { Navigate, useNavigate } from "react-router-dom";
+import { UseSelector, useSelector } from "react-redux";
+
+function PostForm({ post }) {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "Active",
+      },
+    });
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user.userData);
+
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0]
+        ? appwriteService.uploadFile(data.image[0])
+        : null;
+
+      if (file) {
+        appwriteService.deleteFile(post.featuredImage);
+      }
+      const dbPost = await appwriteService.updatePost(post.$id, {
+        ...data,
+        featureImage: file ? file.$id : undefined,
+      })
+        if(dbPost) {
+          navigate(`/post/${dbPost.$id}`);
+        }
+    }
+        else{
+
+        }
+      }
+    
+    }
+
+  };
+  return <div>PostForm</div>;
+}
+
+export default PostForm;
